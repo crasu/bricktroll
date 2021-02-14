903788858efb0a5bc2e002ac4c7041f279605d25
Christian Kraemer <spam@crasu.de>
2021-02-14 10:03:20 +0100

    move display code

--- src/bricktroll.ino
+++ src/bricktroll.ino
@@ -5,30 +5,13 @@
 #include "config.h"
 #include "controller.h"
 #include "rcmodel.h"
+#include "tft.h"
 
 TTGOClass *watch;
-TFT_eSPI *tft;
-int16_t bg_color;
+
 RcModel* model;
 
 Lpf2Hub legoHub;
-
-void clear_tft()
-{
-    tft->fillScreen(bg_color);
-}
-
-void show_full_screen_message(String msg) {
-    clear_tft();
-    tft->setCursor(0, 0);
-    Serial.println(msg);
-    tft->println(msg);
-}
-
-void show_string(String msg) {
-    Serial.print(msg);
-    tft->print(msg);
-}
 
 void setup()
 {
@@ -46,12 +29,7 @@ void setup()
     TaskHandle_t idle_0 = xTaskGetIdleTaskHandleForCPU(0);
     esp_task_wdt_delete(idle_0);
 
-    bg_color = tft->color565(0, 0, 0);
-    tft->setTextSize(2);
-    tft->fillScreen(bg_color);
-    tft->setCursor(0, 0);
-    tft->setRotation(3);
-
+    tft_init();
     controller_init();
 
     show_full_screen_message("Connecting ");
@@ -61,11 +39,12 @@ void setup()
 
 void loop()
 {
-    /*model->connect();
+    model->connect();
     model->calibrate();
     Position pos =read_joystick();
     model->control(pos);
-    */
     delay(20);
+    //delay(1000);
+    //watch->shutdown();
 }
 
